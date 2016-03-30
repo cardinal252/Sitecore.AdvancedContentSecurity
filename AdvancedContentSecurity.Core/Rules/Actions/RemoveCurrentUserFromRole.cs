b3ad996ee810717d203156eb88451aa8a4c5f6ac
Nathanael Mann <nathanael.mann@cit-design.co.uk>
2016-03-30 19:01:24 +0100

    Added user initialisation functionality

---
+++ AdvancedContentSecurity.Core/Rules/Actions/RemoveCurrentUserFromRole.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Rules;
+using Sitecore.Rules.Actions;
+
+namespace AdvancedContentSecurity.Core.Rules.Actions
+{
+    public class RemoveCurrentUserToRole<T> : RuleAction<T> where T : RuleContext
+    {
+        public string Role { get; set; }
+
+        public override void Apply(T ruleContext)
+        {
+            if (Sitecore.Security.Accounts.Role.Exists(Role))
+            {
+                return;
+            }
+
+            var role = Sitecore.Security.Accounts.Role.FromName(Role);
+
+            if (Sitecore.Context.User.Roles.Contains(role))
+            {
+                Sitecore.Context.User.Roles.Remove(role);
+            }
+        }
+    }
+}
